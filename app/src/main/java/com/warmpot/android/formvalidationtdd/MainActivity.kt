3c5586838f11d8a010bc0ebc396dcae37416b5c4
Sunghun Park <mark.sunghun.park@gmail.com>
2021-01-16 18:03:53 +1100

    apply view binding

--- app/src/main/java/com/warmpot/android/formvalidationtdd/MainActivity.kt
+++ app/src/main/java/com/warmpot/android/formvalidationtdd/MainActivity.kt
@@ -4,88 +4,94 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import com.google.android.material.textfield.TextInputLayout
+import com.warmpot.android.formvalidationtdd.databinding.ActivityMainBinding
 import com.warmpot.android.formvalidationtdd.validator.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val usernameTil: TextInputLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.usernameTil) }
-    private val emailTil: TextInputLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.emailTil) }
-    private val mobileTil: TextInputLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.mobileTil) }
-    private val usernameEdt: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.usernameEdt) }
-    private val emailEdt: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.emailEdt) }
-    private val mobileEdt: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.mobileEdt) }
-    private val contactMethodErrorTxt: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.contactMethodErrorTxt) }
-    private val preferredContactMethodGroup: RadioGroup by lazy(LazyThreadSafetyMode.NONE) {
-        findViewById(
-            R.id.preferredContactMethodGroup
-        )
+    private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
     }
-    private val emailRbtn: RadioButton by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.emailRbtn) }
-    private val mobileRbtn: RadioButton by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.mobileRbtn) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        initFormValidator()
+        initViewListeners()
     }
 
-    private lateinit var formValidator: FormValidator
-
-    private fun initFormValidator() {
-        val usernameField = TextField(
-            getText = { usernameEdt.text() },
-            isRequired = { true },
-            onShowError = { error -> usernameTil.error = error },
-            validators = listOf(
-                RequiredFieldValidator(
-                    message = getString(R.string.username_required_message)
+    // region form fields
+    private val usernameField by lazy(LazyThreadSafetyMode.NONE) {
+        binding.run {
+            TextField(
+                getText = { usernameEdt.text() },
+                isRequired = { true },
+                onShowError = { error -> usernameTil.error = error },
+                validators = listOf(
+                    RequiredFieldValidator(
+                        message = getString(R.string.username_required_message)
+                    )
                 )
             )
-        )
+        }
+    }
 
-        val emailField = TextField(
-            getText = { emailEdt.text() },
-            isRequired = { emailRbtn.isChecked },
-            onShowError = { error -> emailTil.error = error },
-            validators = listOf(
-                RequiredFieldValidator(
-                    message = getString(R.string.email_required_message),
-                ),
-                PatternFieldValidator(
-                    pattern = PatternFieldValidator.EMAIL_PATTERN,
-                    message = getString(R.string.email_invalid_format_message)
+    private val emailField by lazy(LazyThreadSafetyMode.NONE) {
+        binding.run {
+            TextField(
+                getText = { emailEdt.text() },
+                isRequired = { emailRbtn.isChecked },
+                onShowError = { error -> emailTil.error = error },
+                validators = listOf(
+                    RequiredFieldValidator(
+                        message = getString(R.string.email_required_message),
+                    ),
+                    PatternFieldValidator(
+                        pattern = PatternFieldValidator.EMAIL_PATTERN,
+                        message = getString(R.string.email_invalid_format_message)
+                    )
                 )
             )
-        )
+        }
+    }
 
-        val mobileField = TextField(
-            getText = { mobileEdt.text() },
-            isRequired = { mobileRbtn.isChecked },
-            onShowError = { error -> mobileTil.error = error },
-            validators = listOf(
-                RequiredFieldValidator(
-                    message = getString(R.string.mobile_required_message)
-                ),
-                PatternFieldValidator(
-                    pattern = PatternFieldValidator.AUSTRALIAN_MOBILE_PATTERN,
-                    message = getString(R.string.mobile_invalid_format_message)
+    private val mobileField by lazy(LazyThreadSafetyMode.NONE) {
+        binding.run {
+            TextField(
+                getText = { mobileEdt.text() },
+                isRequired = { mobileRbtn.isChecked },
+                onShowError = { error -> mobileTil.error = error },
+                validators = listOf(
+                    RequiredFieldValidator(
+                        message = getString(R.string.mobile_required_message)
+                    ),
+                    PatternFieldValidator(
+                        pattern = PatternFieldValidator.AUSTRALIAN_MOBILE_PATTERN,
+                        message = getString(R.string.mobile_invalid_format_message)
+                    )
                 )
             )
-        )
+        }
+    }
 
-        val contactMethodField = RadioGroupField(
-            isRequired = { true },
-            isChecked = { !preferredContactMethodGroup.nothingSelected() },
-            onShowError = { error -> contactMethodErrorTxt.text = error },
-            validators = listOf(
-                RequiredFieldValidator(
-                    message = getString(R.string.contact_method_select_message)
+    private val contactMethodField by lazy(LazyThreadSafetyMode.NONE) {
+        binding.run {
+            RadioGroupField(
+                isRequired = { true },
+                isChecked = { !preferredContactMethodGroup.nothingSelected() },
+                onShowError = { error -> contactMethodErrorTxt.text = error },
+                validators = listOf(
+                    RequiredFieldValidator(
+                        message = getString(R.string.contact_method_select_message)
+                    )
                 )
             )
-        )
+        }
+    }
+    // endregion
 
-        formValidator = FormValidator(
+    private val formValidator by lazy(LazyThreadSafetyMode.NONE) {
+        FormValidator(
             listOf(
                 usernameField,
                 emailField,
@@ -93,16 +99,17 @@ class MainActivity : AppCompatActivity() {
                 contactMethodField
             )
         )
+    }
 
+    private fun initViewListeners() = binding.apply {
         usernameEdt.addTextChangedListener(TextChangedListener(usernameField))
         emailEdt.addTextChangedListener(TextChangedListener(emailField))
         mobileEdt.addTextChangedListener(TextChangedListener(mobileField))
-
         preferredContactMethodGroup.setOnCheckedChangeListener { _, _ ->
             formValidator.validateForm()
         }
 
-        findViewById<Button>(R.id.saveBtn).setOnClickListener {
+        saveBtn.setOnClickListener {
             formValidator.validateForm()
         }
     }
